2862de1d5b9c5fa469ee887cbf465f94ceb2b2b2
Vincenzo Ciaccio <vincenzo@rushpartners.co.uk>
2019-04-22 11:22:40 +0100

    first tests with mocked data

--- frontend/src/components/common/Card.js
+++ frontend/src/components/common/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = ({ image = null, title, subtitle, tail, onClick = () => { } }) => {
 
     return (
-        <div className="max-w-sm rounded overflow-hidden shadow-md my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4">
+        <div className="max-w-sm rounded overflow-hidden shadow-md my-2 px-2 w-full overflow-hidden sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-primary">
             {image && <img className="w-full" src={image} alt={title} />}
             <div className="px-6 py-4">
                 <div className="font-bold text-xl mb-2">{title}</div>
--- frontend/src/components/misc/Body.js
+++ frontend/src/components/misc/Body.js
@@ -1,26 +1,14 @@
 import React, { Fragment } from 'react';
 import { Card } from '../common';
 
+import mock from './mock.json';
+
 const Body = () => (
     <div className="px-4 pt-4 flex-1 overflow-y-scroll">
         <div className="container mx-auto">
             <Card.Grid>
                 <Fragment>
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
-                    <Card title="La Zanzara" subtitle="tutto il resto è noia, no, non ho detto gioia!" image="http://www.radio24.ilsole24ore.com//assets/img/Radio24/_Immagini/2018/01/cruciani-itunes.jpg" />
+                    {mock.payload.map(s => <Card key={s.id} title={s.name} subtitle={s.description} image={s.logo_url} />)}
                 </Fragment>
             </Card.Grid>
         </div>
