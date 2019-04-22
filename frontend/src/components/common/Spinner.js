6338c66793fcd86dd0997c156562dbc24abce8f9
Vincenzo Ciaccio <vincenzo@rushpartners.co.uk>
2019-04-22 18:03:22 +0100

    implemented animated spinner

--- frontend/src/components/common/Spinner.js
+++ frontend/src/components/common/Spinner.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import './Icon.css';
+
+import './Spinner.css';
+
+import logo from '../../resources/images/main-logo.svg';
 
 const Spinner = () => {
-    return <span>Loading...</span>
+    return (
+        <div className="flex justify-center items-center flex-col">
+            <img className="spinner animated rotate mt-20" src={logo} alt="Loading..." />
+            <span className="text-3xl text-accent mt-20" >Loading...</span>
+        </div>
+    );
 };
 
 
