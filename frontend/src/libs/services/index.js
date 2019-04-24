ee2d42b3c22a2bf1455ead2c6bb8a255509fa697
Vincenzo Ciaccio <vincenzo@rushpartners.co.uk>
2019-04-24 22:10:35 +0100

    making api url env dependent

--- frontend/src/libs/services/index.js
+++ frontend/src/libs/services/index.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+import { API_URL } from '../../config';
+
 const http = axios.create({
-    baseURL: 'http://localhost:3001',
+    baseURL: API_URL,
     timeout: 10000
 });
 
